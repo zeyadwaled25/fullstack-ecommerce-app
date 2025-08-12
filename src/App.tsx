b88b571b666d98a014ce825e3eba902d2e0dcb31
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-08-12 23:16:15 +0300

    feet: create navbar layout with dark, light mode and avatar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages";
 import ProductsPage from "./pages/Products";
 import ProductPage from "./pages/SingelProduct";
+import Navbar from "./layout/Navbar";
 
 const App = () => {
   return (
     <>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/products" element={<ProductsPage/>}/>
