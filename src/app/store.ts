7b8d2a36e143eab89b41f9486554e5ed986d5e58
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-08-16 03:33:43 +0300

    feet: handle shopping cart persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,14 +2,25 @@ import { configureStore } from '@reduxjs/toolkit';
 import loginSlice from './features/login/loginSlice';
 import cartSlice from './features/cart/cartSlice';
 import globalSlice from './features/globalSlice';
+import {persistStore, persistReducer} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+const persistCartConfig = {
+  key: 'cart',
+  storage
+}
+
+const persistedCart = persistReducer(persistCartConfig, cartSlice)
 
 export const store = configureStore({
   reducer: {
     login: loginSlice,
-    cart: cartSlice,
+    cart: persistedCart,
     global: globalSlice
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persister = persistStore(store)
