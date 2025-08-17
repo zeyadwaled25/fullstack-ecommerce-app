fe6b70a2333a45cd2d8d13eda1d3462f162f42df
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-08-18 02:24:44 +0300

    feat: add Alert Dialog for user confirmation

--- src/components/DashboardProductsTable.tsx
+++ src/components/DashboardProductsTable.tsx
@@ -1,79 +1,87 @@
 import { 
   Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr, Image, 
   HStack,
-  Button
+  Button,
+  useDisclosure
 } from "@chakra-ui/react";
 import DashboardProductsTableSkeleton from "./DashboardProductsTableSkeleton";
 import { useGetDashboardProductsQuery } from "../app/services/apiSlice";
 import type { IProduct } from "../interfaces";
+import CustomAlertDialog from "../shared/AlertDialog";
 
 const DashboardProductsTable = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const { isLoading, data, error } = useGetDashboardProductsQuery(undefined);
 
   if (isLoading) return <DashboardProductsTableSkeleton />;
   if (error) return <p>Error loading products</p>;
 
   return (
-    <TableContainer maxW={'85%'} mx={'auto'}>
-      <Table variant="simple">
-        <TableCaption>All Products Dashboard</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>ID</Th>
-            <Th>Thumbnail</Th>
-            <Th>Title</Th>
-            <Th isNumeric>Price</Th>
-            <Th>Category</Th>
-            <Th isNumeric>Stock</Th>
-            <Th>Actions</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data?.data?.map((product: IProduct, idx: number) => (
-            <Tr key={product.id}>
-              <Td>{idx}</Td>
-              <Td>
-                <Image
-                  src={`${import.meta.env.VITE_SERVER_URL}${product.thumbnail.url}`}
-                  alt={product.title} 
-                  boxSize="50px" 
-                  objectFit="cover" 
-                  borderRadius="md" 
-                />
-              </Td>
-              <Td>{product.title}</Td>
-              <Td isNumeric>${product.price}</Td>
-              <Td>{product.category.title}</Td>
-              <Td isNumeric>{product.stock}</Td>
-              <Td>
-                <HStack spacing={2}>
-                  <Button size="sm" colorScheme="blue" variant="outline">
-                    View
-                  </Button>
-                  <Button size="sm" colorScheme="yellow" variant="outline">
-                    Edit
-                  </Button>
-                  <Button size="sm" colorScheme="red" variant="outline">
-                    Delete
-                  </Button>
-                </HStack>
-              </Td>
+    <>
+      <TableContainer maxW={'85%'} mx={'auto'}>
+        <Table variant="simple">
+          <TableCaption>All Products Dashboard</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Thumbnail</Th>
+              <Th>Title</Th>
+              <Th isNumeric>Price</Th>
+              <Th>Category</Th>
+              <Th isNumeric>Stock</Th>
+              <Th>Actions</Th>
             </Tr>
-          ))}
-        </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th>ID</Th>
-            <Th>Thumbnail</Th>
-            <Th>Title</Th>
-            <Th isNumeric>Price</Th>
-            <Th>Category</Th>
-            <Th isNumeric>Stock</Th>
-            <Th>Actions</Th>
-          </Tr>
-        </Tfoot>
-      </Table>
-    </TableContainer>
+          </Thead>
+          <Tbody>
+            {data?.data?.map((product: IProduct, idx: number) => (
+              <Tr key={product.id}>
+                <Td>{idx}</Td>
+                <Td>
+                  <Image
+                    src={`${import.meta.env.VITE_SERVER_URL}${product.thumbnail.url}`}
+                    alt={product.title} 
+                    boxSize="50px" 
+                    objectFit="cover" 
+                    borderRadius="md" 
+                  />
+                </Td>
+                <Td>{product.title}</Td>
+                <Td isNumeric>${product.price}</Td>
+                <Td>{product.category.title}</Td>
+                <Td isNumeric>{product.stock}</Td>
+                <Td>
+                  <HStack spacing={2}>
+                    <Button size="sm" colorScheme="blue" variant="outline">
+                      View
+                    </Button>
+                    <Button size="sm" colorScheme="yellow" variant="outline">
+                      Edit
+                    </Button>
+                    <Button size="sm" colorScheme="red" variant="outline"
+                      onClick={onOpen}
+                    >
+                      Delete
+                    </Button>
+                  </HStack>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Thumbnail</Th>
+              <Th>Title</Th>
+              <Th isNumeric>Price</Th>
+              <Th>Category</Th>
+              <Th isNumeric>Stock</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+      <CustomAlertDialog isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+    </>
   );
 };
 
---
+++ src/shared/AlertDialog.tsx
@@ -0,0 +1,43 @@
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button } from "@chakra-ui/react"
+import { useRef } from "react"
+
+interface IProps {
+  isOpen: boolean,
+  onOpen: () => void,
+  onClose: () => void
+}
+
+export default function CustomAlertDialog({isOpen, onOpen, onClose}: IProps) {
+  const cancelRef = useRef(null)
+
+  return (
+    <>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Delete Customer
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='red' onClick={onClose} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  )
+}
