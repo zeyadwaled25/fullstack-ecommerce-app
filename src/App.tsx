9f280140af1a40556f00e5b37cdabc1a83f1a576
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-08-17 23:27:59 +0300

    feet: dashboard layout startup

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import LoginPage from "./pages/LoginPage";
 import AppLayout from "./layout/AppLayout";
 import CookieService from "./services/CookieService";
 import CartDrawer from "./components/CartDrawer";
+import DashboardLayout from "./pages/dashboard/DashboardLayout";
+import AdminDashboard from "./pages/dashboard";
 
 const App = () => {
   const token = CookieService.get('jwt');
@@ -18,6 +20,11 @@ const App = () => {
           <Route index element={<HomePage/>}/>
           <Route path="/products" element={<ProductsPage/>}/>
           <Route path="/products/:documentId" element={<ProductPage/>}/>
+        </Route>
+        <Route path="/dashboard" element={<DashboardLayout/>}>
+          <Route index element={<AdminDashboard/>}/>
+          <Route path="products" element={<h1>Hi Products</h1>}/>
+          <Route path="categories" element={<h1>Hi Categories</h1>}/>
         </Route>
 
         <Route path="/login" element={<LoginPage isAuthenticated={token}/>}/>
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,7 @@
+const AdminDashboard = () => {
+  return (
+    <h2>AdminDashboard</h2>
+  );
+}
+
+export default AdminDashboard;
